697d3d7e19e0a9a19090b44dd9370d7f1d3b848f
valentinacruzo <101739631+valentinacruzo@users.noreply.github.com>
2022-04-26 13:23:52 -0500

    Update guessinggame.sh

--- guessinggame.sh
+++ guessinggame.sh
@@ -3,41 +3,43 @@
 # Date     : April 26 2022
 # ID       : 554801
 subdirectories() {
-	printf "Guessing Game in Bash"
-	while read -r -p "Include subdirectories? [y/n] " subdirs; do
+	printf "Guessing Game"
+	while read -r -p "Include subdirectories? [y/n] " subdirs; 
+		do
 		if [[ "$subdirs" =~ ^([yY][eE][sS]|[yY])$ ]]; then
 			numfiles=$(find . ! -type d | grep -v '.git' -c)
 			subdirsv="current directory and subdirectories:"
-			printf "\033[32m%s \n\n\033[0m" "Ok, subdirectories add!"
+			printf "\033[32m%s \n\033[0m" "Ok, subdirectories added!"
 			break;
 		elif [[ "$subdirs" =~ ^([nN][oO]|[nN])$ ]]; then
 			numfiles=$(find . -maxdepth 1 ! -type d | wc -l)
 			subdirsv="current directory only:"
-			printf "\033[32m%s \n\n\033[0m" "Ok, subdirectories cut out!"
+			printf "\033[32m%s \n\033[0m" "Ok, subdirectories cut out!"
 			break;
 		else
-			printf "\033[31m%s \n\033[0m" "Wrong answer..."
+			printf "\033[31m%s \n\033[0m" "Wrong answer :c"
 		fi
 	done
 }
  
 guessing() {
-	printf "Guess the number of files of the %s\n\n" "$subdirsv"
+	printf "Guess the number of files of the %s\n" "$subdirsv"
 	attempts=1
-	while read -r -p "Attempt $attempts → Type a number: " answer; do
+	while read -r -p "Attempt $attempts → Type a number: " answer; 
+		do
 		if [[ "$answer" =~ ^[0-9]+$ ]]; then
 			if [[ "$answer" -lt "$numfiles" ]] 2>/dev/null; then
-				printf "\033[33m%s \n\n\033[0m" "Too low!"
+				printf "\033[33m%s \n\033[0m" "Too low! :o"
 				attempts=$((attempts + 1))
 			elif [[ "$answer" -gt "$numfiles" ]] 2>/dev/null; then
-				printf "\033[33m%s \n\n\033[0m" "Too high!"
+				printf "\033[33m%s \n\033[0m" "Too high! :O"
 				attempts=$((attempts + 1))
 			elif [ "$answer" -eq "$numfiles" ] 2>/dev/null; then
-				printf "\033[32m%s \n\n\033[0m" "Congratulations! You win!"
+				printf "\033[32m%s \n\033[0m" "Congratulations! :3 Easy win!"
 				exit 0
 			fi
 		else
-			printf "\033[31m%s \n\n\033[0m" "Wrong answer..."
+			printf "\033[31m%s \n\033[0m" "Wrong answer :c"
 			attempts=$((attempts + 1))
 		fi
 	done
